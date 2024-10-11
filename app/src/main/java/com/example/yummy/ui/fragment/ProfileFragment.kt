539fbeee626ff8d67d916a438e3bfd545f272093
osman <osefe47@gmail.com>
2024-10-11 17:27:59 +0300

    Update

--- app/src/main/java/com/example/yummy/ui/fragment/ProfileFragment.kt
+++ app/src/main/java/com/example/yummy/ui/fragment/ProfileFragment.kt
@@ -3,8 +3,12 @@ package com.example.yummy.ui.fragment
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.yummy.R
 import com.example.yummy.databinding.FragmentProfileBinding
 
@@ -15,13 +19,13 @@ class ProfileFragment : Fragment() {
         super.onCreate(savedInstanceState)
 
     }
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         binding=FragmentProfileBinding.inflate(inflater,container,false)
+
+
         return binding.root
     }
+
 
 }
